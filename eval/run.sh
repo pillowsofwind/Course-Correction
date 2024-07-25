b282a2188acfc3247cd5a911e2f5f87dd22b5952
pillowsofwind <rongwuxu@outlook.com>
2024-07-25 12:06:37 +0800

    minor fix

--- eval/run.sh
+++ eval/run.sh
@@ -1,3 +1,4 @@
-
+mkdir output
+mkdir raw_results
 python eval_data.py --model llama2 ## specify your model here, valid option = ["llama2","vicuna","zephyr","llama3","glm","qwen_05","qwen_15","qwen_7","qwen_72","phi"]
 python gpteval.py --model llama2 --openai_key your_api_key_here #please specify the model, valid option = ["llama2","vicuna","zephyr","llama3","glm","qwen_05","qwen_15","qwen_7","qwen_72","phi"], and your openai API key
